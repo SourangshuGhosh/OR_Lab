ceef466d7a89b2fd4a3dcd2c66b6c7d2ad1a8dcf
Sourangshu Ghosh <sourangshug123@gmail.com>
2021-02-07 21:15:09 +0530

    Index and Readme Files

---
+++ or_lab.sh
@@ -0,0 +1,41 @@
+# Hack to run c / c++ files with 1 command like Python / Ruby
+# Like `or_c filename.c` or `or_cpp filename.cpp`
+function or_c {
+gcc $1 -o $1.out -std=c99
+./$1.out
+ls
+}
+
+function or_cpp {
+g++ $1 -o $1.out
+./$1.out
+ls
+}
+
+# Hack to push all files to git with 1 command.
+function or_push {
+cd $OR_LAB_REPO_PATH
+git add -A
+git commit -a -m "Deployed by bash : $1"
+git push origin master
+ls
+}
+
+# Hack to pull all files to git with 1 command. Assumes no merge conflictss. Dies otherwise.
+function or_pull {
+cd $OR_LAB_REPO_PATH
+git add -A
+git stash
+git pull origin master
+}
+
+# Too lazy to type 6 extra characters
+function or_cd {
+cd $OR_LAB_REPO_PATH
+}
+
+# Hack : cd = cd + ls
+function cd {
+builtin cd $1
+ls
+}
